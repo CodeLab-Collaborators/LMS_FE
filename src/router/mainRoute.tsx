6ffc5fc26e9b19e454fee28a55901b37eb4a129b
Peter Oti <71887594+PetxCode@users.noreply.github.com>
2023-08-13 10:39:45 +0100

    LMS

---
+++ src/router/mainRoute.tsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter } from "react-router-dom"
+import Register from "../pages/auth/Register"
+import HomeScreen from "../pages/screen/home/HomeScreen"
+import ErrorScreen from "../error/error2/Error"
+import SignInScreen from "../pages/auth/SigninScreen"
+import PrivateRoute from "./privateRoute"
+import Layout from "../components/common/Layout"
+import TestScreen from "../pages/screen/test/TestScreen"
+import TestScreen2 from "../pages/screen/test/TestScreen2"
+import ErrorImport from "../error/error/ErrorImport"
+
+export const mainRoute = createBrowserRouter([
+    {
+        path: "register",
+        element: <Register />
+    },
+    {
+        path: "/sign-in",
+        element: <SignInScreen />
+    },
+    {
+        path: "/",
+        element: <PrivateRoute>
+            <Layout />
+        </PrivateRoute>,
+        children: [
+            {
+                index: true,
+                element: <HomeScreen />
+            },
+            {
+                index: true,
+                path: "test",
+                element: <TestScreen />
+            },
+            {
+                index: true,
+                path: "error",
+                element: <ErrorImport />
+            },
+            {
+                index: true,
+                path: "test2",
+                element: <TestScreen2 />
+            }
+        ]
+
+
+    },
+    {
+        path: "*",
+        element: <ErrorScreen />
+    },
+])
